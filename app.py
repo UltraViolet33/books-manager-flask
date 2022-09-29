7f87b2b54c14b63494e27cc9760363e050632d06
Ulysse <ulysse.valdenairepro@gmail.com>
2022-09-29 09:39:29 +0200

    add a book

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 
-from flask import Flask, render_template, request, json
+from flask import Flask, render_template, request, json, redirect, session
 from flaskext.mysql import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.secret_key = "This is my secret"
 
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -12,7 +13,6 @@ app.config['MYSQL_DATABASE_DB'] = 'books-rating'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
 mysql.init_app(app)
-
 
 
 @app.route("/")
@@ -27,7 +27,6 @@ def displaySignup():
 
 @app.route("/api/signup", methods=['POST'])
 def signup():
-
     name = request.form['inputName']
     email = request.form['inputEmail']
     password = request.form['inputPassword']
@@ -37,7 +36,7 @@ def signup():
         conn = mysql.connect()
         cursor = conn.cursor()
         cursor.callproc('sp_createUser', (name, email, hashed_password))
-        
+
         data = cursor.fetchall()
         if len(data) == 0:
             conn.commit()
@@ -59,25 +58,17 @@ def validateLogin():
     try:
         username = request.form['inputEmail']
         password = request.form['inputPassword']
-        print(username)
-        print(password)
 
-        
         conn = mysql.connect()
         cursor = conn.cursor()
-        
 
-        cursor.callproc('sp_validateLogin',(username,))
+        cursor.callproc('sp_validateLogin', (username,))
         data = cursor.fetchall()
 
-        print(data)
-
         if len(data) > 0:
-            if check_password_hash(str(data[0][3]),password):
-                print('ok')
-                # session['user'] = data[0][0]
-                return redirect('/home')
-                # return render_template('signin.html', error="OK")
+            if check_password_hash(str(data[0][3]), password):
+                session['user'] = data[0][0]
+                return redirect('/userHome')
             else:
                 return render_template('signin.html', error="wrong email or password")
         else:
@@ -88,6 +79,54 @@ def validateLogin():
     # finally:
     #     cursor.close()
     #     conn.close()
+
+
+@app.route('/userHome')
+def userHome():
+    if session.get('user'):
+        return render_template('userHome.html')
+    else:
+        return redirect("/signin")
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('signin')
+
+
+@app.route('/showAddBook')
+def displayAddBook():
+    if session.get('user'):
+        return render_template('addBook.html')
+    else:
+        redirect('/signin')
+
+
+@app.route('/addBook', methods=['POST'])
+def addBook():
+    if session.get('user'):
+        title = request.form['inputTitle']
+        author = request.form['inputAuthor']
+        category = request.form['inputCategory']
+        rating = request.form['inputRating']
+        comments = request.form['inputComments']
+        user = session.get('user')
+
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.callproc('sp_addBook', (title, author,
+                        category, rating, comments, user))
+        data = cursor.fetchall()
+
+        if len(data) == 0:
+            conn.commit()
+            return redirect('/userHome')
+        else:
+            return render_template('addBook.html', error='error')
+    else:
+        return redirect('/signin')
+
 
 if __name__ == "__main__":
     app.run()
