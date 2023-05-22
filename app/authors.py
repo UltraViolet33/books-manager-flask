3d9e88c8292f33c895e8aecd7b7cd8a80303b4af
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-05-22 16:39:50 +0200

    edit author

--- app/authors.py
+++ app/authors.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db
 from .models.Author import Author
-from flask_login import login_user, logout_user, login_required, current_user
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_required, current_user
 from .forms import AuthorForm
 
 
@@ -12,11 +11,10 @@ authors = Blueprint("authors", __name__)
 @authors.route("/add", methods=["GET", "POST"])
 @login_required
 def add_author():
-
     form = AuthorForm()
 
     if form.validate_on_submit():
-        #check if author already exists
+        # check if author already exists
         author = Author(name=form.name.data, description=form.description.data)
         db.session.add(author)
         db.session.commit()
@@ -24,7 +22,33 @@ def add_author():
         flash(f"Auteur {author.name} ajouté", category="success")
         return redirect(url_for("authors.all_authors"))
 
-    return render_template("authors/add_author.html", user=current_user, form=form)
+    return render_template("authors/author_form.html", user=current_user, form=form)
+
+
+@authors.route("/edit/<id>", methods=["GET", "POST"])
+@login_required
+def edit_author(id):
+    author = Author.query.filter_by(id=id).first()
+
+    if not author:
+        return "404", 404
+
+    form = AuthorForm()
+
+    if request.method == "GET":
+        form.description.data = author.description
+
+    if form.validate_on_submit():
+        # check if author already exists
+        author.name = form.name.data
+        author.description = form.description.data
+
+        db.session.commit()
+
+        flash(f"Auteur {author.name} modifié", category="success")
+        return redirect(url_for("authors.all_authors"))
+
+    return render_template("authors/author_form.html", author=author, user=current_user, form=form)
 
 
 @authors.route("/all")
